44a6b94201c2d85818e21e14391410f9d0460dd0
AIG(ムIG) <58285388+AIGrecords@users.noreply.github.com>
2021-02-25 19:24:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ root = Tk()
 root.title("Clock")
 
 def time():
-  string = strftime('%H:%M:%S %p')
+  string = strftime('%H:%M:%S%p')
   label.config(text=string)
   label.after(1000, time)
-  
+ 
 label = Label(font = ("ds-digital", 80), background = "black", foreground = "red")
 label.pack(anchor = 'center')
 
 time()
-
 mainloop()
